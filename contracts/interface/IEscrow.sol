90a38e35e6efb19ccb3ddd994c918fc157e2fcdd
Thomrich94 <homrich.thiago@gmail.com>
2025-10-13 16:03:15 -0300

    add struct to IEscrow and update foundry.toml to make forge fmt work properly

--- contracts/interface/IEscrow.sol
+++ contracts/interface/IEscrow.sol
@@ -2,14 +2,30 @@
 pragma solidity ^0.8.3;
 
 interface IEscrow {
-  event EscrowCreated(uint256 indexed id, address payer, address payee, uint256 value, bytes32 hashlock, uint64 timelock);
-  event Released(uint256 indexed id, bytes preimage);
-  event Refunded(uint256 indexed id);
-  event Resolved(uint256 indexed id, address winnner);
+    event EscrowCreated(
+        uint256 indexed id,
+        address indexed payer,
+        address indexed payee,
+        uint256 value,
+        bytes32 hashlock,
+        uint64 timelock
+    );
+    event Released(uint256 indexed id, bytes preimage);
+    event Refunded(uint256 indexed id);
+    event Resolved(uint256 indexed id, address winner);
 
-  function createEscrow(address payee, bytes32 hashlock, uint64 timelock) external payable returns(uint256 id);
-  function release(uint256 id, bytes calldata preimage) external;
-  function refund(uint256 id) external;
-  function resolve(uint256 id, bool toPayee) external;
-  function getEscrow(uint256 id) external view /* returns (struct fields ) */;
+    struct EscrowDetails {
+        address payer;
+        address payee;
+        uint256 value;
+        bytes32 hashlock;
+        uint64 timelock;
+        uint8 status; // 0:Created, 1:Released, 2:Refunded, 3:Resolved
+    }
+
+    function createEscrow(address payee, bytes32 hashlock, uint64 timelock) external payable returns (uint256 id);
+    function release(uint256 id, bytes calldata preimage) external;
+    function refund(uint256 id) external;
+    function resolve(uint256 id, bool toPayee) external;
+    function getEscrow(uint256 id) external view returns (EscrowDetails memory);
 }
