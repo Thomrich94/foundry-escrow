c91c415e0511522c5c3f048c57396176d1306de0
Thomrich94 <homrich.thiago@gmail.com>
2025-10-13 16:33:18 -0300

    implement createEscrow function

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -5,6 +5,8 @@ import { IEscrow } from "./interface/IEscrow.sol";
 
 contract Escrow is IEscrow {
     address public immutable arbiter;
+    uint256 private nextEscrowId;
+    mapping(uint256 => EscrowDetails) private escrows;
 
     error Escrow__ValueMustBePositive();
     error Escrow__InvalidPayee();
@@ -21,7 +23,31 @@ contract Escrow is IEscrow {
         arbiter = _arbiter;
     }
 
-    function createEscrow(address payee, bytes32 hashlock, uint64 timelock) external payable returns (uint256 id) {}
+    function createEscrow(address payee, bytes32 hashlock, uint64 timelock) external payable returns (uint256 id) {
+        if (msg.value == 0) {
+            revert Escrow__ValueMustBePositive();
+        }
+        if (payee == address(0)) {
+            revert Escrow__InvalidPayee();
+        }
+        if (timelock <= block.timestamp) {
+            revert Escrow__TimelockNotInFuture(timelock, uint64(block.timestamp));
+        }
+
+        id = nextEscrowId;
+        escrows[id] = EscrowDetails({
+          payer: msg.sender,
+          payee: payee,
+          value: msg.value,
+          hashlock: hashlock,
+          timelock: timelock,
+          status: uint8(Status.Created)
+        });
+
+        nextEscrowId++;
+        emit EscrowCreated(id, msg.sender, payee, msg.value, hashlock, timelock);
+    }
+    
     function release(uint256 id, bytes calldata preimage) external {}
     function refund(uint256 id) external {}
     function resolve(uint256 id, bool toPayee) external {}
