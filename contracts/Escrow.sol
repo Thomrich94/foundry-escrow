e90c97d115da7f4ea3d68f400c7508b77c22c56c
Thomrich94 <homrich.thiago@gmail.com>
2025-10-28 15:14:32 -0300

    remove _ from params since we switched to chainlink way of naming state variables

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -19,17 +19,17 @@ contract Escrow is IEscrow {
     error Escrow__TimelockNotExpired(uint256 blockTimestamp, uint64 escrowTimelock);
     error Escrow__NotArbiter(address caller, address arbiter);
 
-    modifier escrowExists(uint256 _id) {
-        if (s_escrows[_id].payer == address(0)) {
-            revert Escrow__NotFound(_id);
+    modifier escrowExists(uint256 id) {
+        if (s_escrows[id].payer == address(0)) {
+            revert Escrow__NotFound(id);
         }
         _;
     }
 
-    modifier inStatus(uint256 _id, Status _requiredStatus) {
-        uint8 currentStatus = s_escrows[_id].status;
-        if (currentStatus != uint8(_requiredStatus)) {
-            revert Escrow__InvalidState(_id, currentStatus, uint8(_requiredStatus));
+    modifier inStatus(uint256 id, Status requiredStatus) {
+        uint8 currentStatus = s_escrows[id].status;
+        if (currentStatus != uint8(requiredStatus)) {
+            revert Escrow__InvalidState(id, currentStatus, uint8(requiredStatus));
         }
         _;
     }
@@ -48,53 +48,53 @@ contract Escrow is IEscrow {
         Resolved
     }
 
-    constructor(address _arbiter) {
-        i_arbiter = _arbiter;
+    constructor(address arbiter) {
+        i_arbiter = arbiter;
     }
 
-    function createEscrow(address _payee, bytes32 _hashlock, uint64 _timelock) external payable returns (uint256 id) {
+    function createEscrow(address payee, bytes32 hashlock, uint64 timelock) external payable returns (uint256 id) {
         if (msg.value == 0) {
             revert Escrow__ValueMustBePositive();
         }
-        if (_payee == address(0)) {
+        if (payee == address(0)) {
             revert Escrow__InvalidPayee();
         }
-        if (_timelock <= block.timestamp) {
-            revert Escrow__TimelockNotInFuture(_timelock, uint64(block.timestamp));
+        if (timelock <= block.timestamp) {
+            revert Escrow__TimelockNotInFuture(timelock, uint64(block.timestamp));
         }
 
         id = s_nextEscrowId;
         s_escrows[id] = EscrowDetails({
             payer: msg.sender,
-            payee: _payee,
+            payee: payee,
             value: msg.value,
-            hashlock: _hashlock,
-            timelock: _timelock,
+            hashlock: hashlock,
+            timelock: timelock,
             status: uint8(Status.Created)
         });
 
         s_nextEscrowId++;
-        emit EscrowCreated(id, msg.sender, _payee, msg.value, _hashlock, _timelock);
+        emit EscrowCreated(id, msg.sender, payee, msg.value, hashlock, timelock);
     }
 
-    function release(uint256 _id, bytes calldata _preimage) external escrowExists(_id) inStatus(_id, Status.Created) {
-        EscrowDetails storage escrow = s_escrows[_id];
+    function release(uint256 id, bytes calldata preimage) external escrowExists(id) inStatus(id, Status.Created) {
+        EscrowDetails storage escrow = s_escrows[id];
 
-        if (keccak256(_preimage) != escrow.hashlock) {
-            revert Escrow__InvalidPreimage(_id);
+        if (keccak256(preimage) != escrow.hashlock) {
+            revert Escrow__InvalidPreimage(id);
         }
 
         escrow.status = uint8(Status.Released);
-        emit EscrowReleased(_id, _preimage);
+        emit EscrowReleased(id, preimage);
 
         (bool success,) = escrow.payee.call{ value: escrow.value }("");
         if (!success) {
-            revert Escrow__TransferFailed(_id, escrow.payee, escrow.value);
+            revert Escrow__TransferFailed(id, escrow.payee, escrow.value);
         }
     }
 
-    function refund(uint256 _id) external escrowExists(_id) inStatus(_id, Status.Created) {
-        EscrowDetails storage escrow = s_escrows[_id];
+    function refund(uint256 id) external escrowExists(id) inStatus(id, Status.Created) {
+        EscrowDetails storage escrow = s_escrows[id];
 
         if (msg.sender != escrow.payer) {
             revert Escrow__NotThePayer(msg.sender, escrow.payer);
@@ -104,25 +104,25 @@ contract Escrow is IEscrow {
         }
 
         escrow.status = uint8(Status.Refunded);
-        emit EscrowRefunded(_id);
+        emit EscrowRefunded(id);
 
         (bool success,) = escrow.payer.call{ value: escrow.value }("");
         if (!success) {
-            revert Escrow__TransferFailed(_id, escrow.payer, escrow.value);
+            revert Escrow__TransferFailed(id, escrow.payer, escrow.value);
         }
     }
 
-    function resolve(uint256 _id, bool _toPayee) external onlyArbiter escrowExists(_id) inStatus(_id, Status.Created) {
-        EscrowDetails storage escrow = s_escrows[_id];
+    function resolve(uint256 id, bool toPayee) external onlyArbiter escrowExists(id) inStatus(id, Status.Created) {
+        EscrowDetails storage escrow = s_escrows[id];
 
-        address winner = _toPayee ? escrow.payee : escrow.payer;
+        address winner = toPayee ? escrow.payee : escrow.payer;
 
         escrow.status = uint8(Status.Resolved);
-        emit EscrowResolved(_id, winner);
+        emit EscrowResolved(id, winner);
 
         (bool success, ) = winner.call{value: escrow.value}("");
         if (!success) {
-            revert Escrow__TransferFailed(_id, winner, escrow.value);
+            revert Escrow__TransferFailed(id, winner, escrow.value);
         }
      }
 
--- contracts/MultisigArbiter.sol
+++ contracts/MultisigArbiter.sol
@@ -2,3 +2,35 @@
 pragma solidity ^0.8.3;
 
 import { IMultisigArbiter } from "./interface/IMultisigArbiter.sol";
+
+contract MultisigArbiter is IMultisigArbiter {
+  address public immutable i_escrowContract;
+  uint256 public immutable i_requiredConfirmations;
+  address[] public s_arbiters;
+  mapping(address => bool) private s_isArbiter;
+
+  struct Resolution {
+    bool exists;
+    bool executed;
+    uint256 confirmationCount;
+    mapping(address => bool) hasConfirmed;
+  }
+
+  mapping(uint256 => mapping(bool => Resolution)) private s_resolutions;
+
+  error MultisigArbiter__NotAnArbiter(address caller);
+  error MultisigArbiter__AlreadyConfirmed(uint256 escrowId, bool toPayee, address confirmer);
+  error MultisigArbiter__ResolutionAlreadyExecuted(uint256 escrowId, bool toPayee);
+  error MultisigArbiter__ResolutionDoesNotExist(uint256 escrowId, bool toPayee);
+  error MultisigArbiter__NotEnoughConfirmations(uint256 escrowId, bool toPayee, uint256 confirmations, uint256 required);
+  error MultisigArbiter__InvalidArbiterList();
+  error MultisigArbiter__InvalidRequiredConfirmations(uint256 required, uint256 arbiters);
+
+  modifier onlyArbiter() {
+    if (!s_isArbiter[msg.sender]) {
+      revert MultisigArbiter__NotAnArbiter(msg.sender);
+    }
+  }
+
+  constructor(address[] memory _arbiters, uint256 _requiredConfirmations, address)
+}
